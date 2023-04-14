4fb916eb23c1b11dd40a2e688af44838aaecae5d
Arden <arden.haldorson@gmail.com>
2023-04-14 17:39:57 +0200

    added chevrons to info boxes

--- src/main.ts
+++ src/main.ts
@@ -21,18 +21,27 @@ const infoBox4 = document.querySelector('#info-box-4') as HTMLDivElement
 const infoBox4Content = document.querySelector('#info-box-4-content') as HTMLDivElement
 const infoBox4Text = document.querySelector('#info-box-4-text') as HTMLElement
 
+// Chevrons
+const chevron1 = document.querySelector('#box-1-chevron') as SVGElement
+const chevron2 = document.querySelector('#box-2-chevron') as SVGElement
+const chevron3 = document.querySelector('#box-3-chevron') as SVGElement
+const chevron4 = document.querySelector('#box-4-chevron') as SVGElement
+
+
 
 /**
  * Function to toggle info boxes from hidden to showing
  * @param boxContent
  * @param boxText
  */
-const toggleBox = (boxContent: HTMLElement, boxText: HTMLElement) => {
+const toggleBox = (boxContent: HTMLElement, boxText: HTMLElement, chevron: SVGElement) => {
 	boxContent.classList.toggle('hidden')
 	boxContent.classList.toggle('box-content')
 
 	boxText.classList.toggle('box-text')
 	boxText.classList.toggle('box-text-toggled')
+
+	chevron.classList.toggle('rotate')
 
 	boxContent.scrollIntoView({ behavior: 'smooth'})
 
@@ -40,25 +49,25 @@ const toggleBox = (boxContent: HTMLElement, boxText: HTMLElement) => {
 
 infoBox1.addEventListener('click', () => {
 
-	toggleBox(infoBox1Content, infoBox1Text)
+	toggleBox(infoBox1Content, infoBox1Text, chevron1)
 
 
 })
 
 infoBox2.addEventListener('click', () => {
 
-	toggleBox(infoBox2Content, infoBox2Text)
+	toggleBox(infoBox2Content, infoBox2Text, chevron2)
 
 })
 
 infoBox3.addEventListener('click', () => {
 
-	toggleBox(infoBox3Content, infoBox3Text)
+	toggleBox(infoBox3Content, infoBox3Text, chevron3)
 
 })
 
 infoBox4.addEventListener('click', () => {
 
-	toggleBox(infoBox4Content, infoBox4Text)
+	toggleBox(infoBox4Content, infoBox4Text, chevron4)
 
 })
